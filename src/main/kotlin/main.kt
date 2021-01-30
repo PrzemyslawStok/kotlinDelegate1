7fe30588757bb4783f233b8da538364d290b21cf
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-01-30 13:07:57 +0100

    cd..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,22 +1,41 @@
-interface computerDrive{
+interface computerDrive {
     val capacity: Double
     val speed: Double
-    fun readData():String
-    fun writeData():String
+    fun readData(): String
+    fun writeData(): String
 }
 
-interface processor{
+class ssd(override val capacity: Double, override val speed: Double) : computerDrive {
+    override fun readData(): String {
+        return "readData - ssd"
+    }
+
+    override fun writeData(): String {
+        return "writeData - ssd"
+    }
 
 }
 
-open class computer(drive: computerDrive, amd: processor): computerDrive by drive{
-    fun start(){
+interface processor {
+    val processorSpeed: Double
+    val noCores: Double
 
+    fun starProgram(): String
+}
+
+class interCorei7(override val processorSpeed: Double, override val noCores: Double) : processor {
+    override fun starProgram(): String {
+        return "startProgram"
+    }
+
+}
+
+open class computer(drive: computerDrive, Processor: processor) : computerDrive by drive, processor by Processor {
+    fun start() {
+        println(readData())
+        println(writeData())
     }
 }
-
-
-
 
 
 fun main() {
