fc5fc725ee5face8426834cb617cd590fce81bb8
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-02-05 19:04:14 +0100

    cd..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -28,7 +28,11 @@ class interCorei7(override val processorSpeed: Double, override val noCores: Dou
     }
 }
 
-class amdRyzen7
+class amdRyzen7(override val processorSpeed: Double, override val noCores: Double) : processor {
+    override fun starProgram(): String {
+        return "startProgram"
+    }
+}
 
 open class computer(drive: computerDrive, Processor: processor) : computerDrive by drive, processor by Processor {
     fun start() {
@@ -36,13 +40,13 @@ open class computer(drive: computerDrive, Processor: processor) : computerDrive 
         println(writeData())
     }
 
-    fun info(){
+    fun info() {
         println("processor speed $processorSpeed")
         println("drive speed $speed")
     }
 }
 
-class server(drive: computerDrive, Processor: processor) : computer(drive, Processor){
+class server(drive: computerDrive, Processor: processor) : computer(drive, Processor) {
 
 }
 
@@ -51,4 +55,8 @@ fun main() {
     val simpleComputer = computer(ssd(2048.0, 500.0), interCorei7(1000.0, 4.0))
     simpleComputer.start()
     simpleComputer.info()
+
+    val nextComputer = computer(ssd(1024.0, 1024.0), amdRyzen7(1500.0, 8.0))
+    nextComputer.start()
+    nextComputer.info()
 }
